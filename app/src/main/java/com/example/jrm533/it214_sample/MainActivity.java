67c6feab06f6405091231bca47d48e924503dc98
Joe Meyer <jrm533@psu.edu>
2018-12-04 20:06:21 -0500

    Removed test interface

--- app/src/main/java/com/example/jrm533/it214_sample/MainActivity.java
+++ app/src/main/java/com/example/jrm533/it214_sample/MainActivity.java
@@ -1,37 +1,16 @@
 package com.example.jrm533.it214_sample;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity
-        extends AppCompatActivity implements Comparable{
+        extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        List<Comparable> someListOfThings = new ArrayList<>();
-
-        someListOfThings.add(this);
-
-
-
     }
-
-    @Override
-    public int compareTo(@NonNull Object o) {
-        return 0;
-    }
-
-
-
-
-
 
 }
